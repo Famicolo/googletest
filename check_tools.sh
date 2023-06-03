7d92d329ed8ad814a597ea1ef351ff327306d2b9
Famicolo <famicolo@gmail.com>
2023-06-03 18:05:06 +0200

    add file sh to check installed tools

---
+++ check_tools.sh
@@ -0,0 +1,10 @@
+if ! command -v g++ &> /dev/null; then
+
+    echo "Il comando g++ non è installato."
+    echo "Installazione di build-essential..."
+else
+    echo "il comando è correttamente installato"
+    #echo $password | sudo -S apt-get update
+    #echo $password | sudo -S apt-get install -y build-essential    # -y da assume yes per evitare le richieste nel procedere con l'installazione
+    # Utilizza l'opzione -y per confermare automaticamente l'installazione
+fi
