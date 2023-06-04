39c91cb31344c614de3ab7596ff7bb2e8d5fdabf
FilippoIspanico <filippo.ispanico@gmail.com>
2023-06-04 22:28:59 +0200

    Aggiunto IO program

---
+++ managefile.cpp
@@ -0,0 +1,28 @@
+#include<iostream> 
+#include<fstream> 
+
+int main(int argc, char* argv[]){
+	
+	if(argc!= 3){
+		std::cerr<<"Error in program " << argv[0]<< "! Wrong number of parameters."<<std::endl; 
+		return -1; 
+	}
+	
+	std::ifstream ist( argv[1] );  
+	
+	if(!ist){
+		std::cerr<<"Program " << argv[0] << " cannot open file:  " << argv[1]<<std::endl; 
+		return -1; 
+	}
+	
+	
+	std::ofstream ost ( argv[2] );   
+	
+	if(!ost){
+		std::cerr<< "Program " << argv[0] << " cannot create file:  " << argv[2]<<std::endl; 
+	}
+	
+	ost << ist.rdbuf(); 
+	
+	return 0;
+}
