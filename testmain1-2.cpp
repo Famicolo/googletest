363c601a2c1abd3a6437ac5fa840c59a408d503c
FilippoIspanico <133004373+FilippoIspanico@users.noreply.github.com>
2023-06-07 23:41:33 +0200

    Update testmain1-2.cpp

--- testmain1-2.cpp
+++ testmain1-2.cpp
@@ -3,7 +3,7 @@ Execute sampletest for samples1 and samples2
 
 compiler call:
 
-g++ -o testmain1-2 testmain1-2.cpp googletest/samples/sample1_unittest.cc googletest/samples/sample2_unittest.cc googletest/samples/sample1.cc  googletest/samples/sample2.cc  -lgtest -lpthread
+g++ -o testmain1-2 testmain1-2.cpp googletest/samples/sample1_unittest.cc googletest/samples/sample2_unittest.cc googletest/samples/sample1.cc  googletest/samples/sample2.cc googletest/samples/sample4_unittest.cc googletest/samples/sample4.cc  -lgtest -lpthread
 
 */
 
@@ -15,6 +15,8 @@ g++ -o testmain1-2 testmain1-2.cpp googletest/samples/sample1_unittest.cc google
 
 #include"googletest/samples/sample1_unittest.cc"
 #include"googletest/samples/sample2_unittest.cc"
+#include"googletest/samples/sample4_unittest.cc"
+
 
 int main(int argc, char* argv[]){
   testing::InitGoogleTest(&argc, argv); 
